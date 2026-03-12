7301d889feddc4741d4ed19eed81335e9f70d07e
Yadiel Contreras <rubencp.yadielghz@gmail.com>
2026-03-12 12:45:29 -0600

    Migre circular gallery a la carpeta de servicios e importe con la nueva ruta en accesorios

--- front/src/app/components/pages/accesorios/accesorios.component.ts
+++ front/src/app/components/pages/accesorios/accesorios.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CircularGalleryApp } from './circular-gallery.service';
+import { CircularGalleryApp } from '../../../services/circular-gallery.service';
 import { ProductModalComponent, Product } from '../../product-modal/product-modal.component';
 import { TimbresModalComponent } from '../../shared/timbres-modal/timbres-modal.component';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
